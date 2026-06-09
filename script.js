5d99db8c7e014457ca23d139e8f5097da1182b8b
rooot16 <948142627@qq.com>
2026-06-09 02:01:17 -0400

    Modified

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(id) {
+
+    document.querySelectorAll('.section')
+        .forEach(section => {
+            section.classList.remove('active');
+        });
+
+    document.getElementById(id)
+        .classList.add('active');
+
+}
